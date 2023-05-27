6693b7e22a8620c051816716b1f216b1102ba6ae
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-05-27 21:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', ()=>{
+	document.getElementById('status').style.display='none';
+	document.querySelector('.entered').style.display='block';
+})
