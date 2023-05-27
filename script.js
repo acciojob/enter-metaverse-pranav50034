47a1ced1579b0065252d3dbd62e25c375d45727e
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-05-27 21:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.getElementById('enterBtn').addEventListener('click', ()=>{
-	document.getElementById('status').style.display='none';
-	document.querySelector('.entered').style.display='block';
+	document.getElementById('status').style.innerText='Entered Metaverse';
+	// document.querySelector('.entered').style.display='block';
 })
